139f429cec3b73aabd024f5486d0bd07372b1c5f
Sergey Belov <peimei@ya.ru>
2012-11-21 11:36:38 +0200

    Refactor bemhtml tech module to use recommended tech API

--- .bem/techs/bemhtml.js
+++ .bem/techs/bemhtml.js
@@ -1,52 +1,53 @@
 var BEM = require('bem'),
     BEMC = require('bemc'),
     Q = BEM.require('q'),
-    PATH = require('path'),
-    SYS = require('util'),
+    PATH = require('path');
 
-    readFile = BEM.require('./util').readFile;
+exports.techMixin = {
 
-exports.getBuildResultChunk = function(relPath, path, suffix) {
+    getBuildResultChunk: function(relPath, path, suffix) {
 
-    return readFile(path)
-        .then(function(c) {
+        return BEM.util.readFile(path)
+            .then(function(c) {
 
-            return [
-                '/* ' + path + ': start */',
-                c,
-                '/* ' + path + ': end */',
-                '\n'
-            ].join('\n');
+                return [
+                    '/* ' + path + ': start */',
+                    c,
+                    '/* ' + path + ': end */',
+                    '\n'
+                ].join('\n');
 
-        });
-
-};
-
-exports.getBuildResult = function(prefixes, suffix, outputDir, outputName) {
-
-    var _this = this;
-    return this.filterPrefixes(prefixes, this.getCreateSuffixes())
-        .then(function(paths) {
-            return Q.all(paths.map(function(path) {
-                return _this.getBuildResultChunk(
-                    PATH.relative(outputDir, path), path, suffix);
-            }));
-        })
-        .then(function(sources) {
-            sources = sources.join('\n');
-
-            return BEMC.translate(sources, {
-                exportName: 'BEMHTML',
-                devMode: process.env.BEMHTML_ENV == 'development'
             });
-        });
 
-};
+    },
 
-exports.getSuffixes = function() {
-    return ['bemhtml'];
-};
+    getBuildResult: function(prefixes, suffix, outputDir, outputName) {
 
-exports.getBuildSuffixes = function() {
-    return ['bemhtml.js'];
+        var _this = this;
+        return this.filterPrefixes(prefixes, this.getCreateSuffixes())
+            .then(function(paths) {
+                return Q.all(paths.map(function(path) {
+                    return _this.getBuildResultChunk(
+                        PATH.relative(outputDir, path), path, suffix);
+                }));
+            })
+            .then(function(sources) {
+                sources = sources.join('\n');
+
+                return BEMC.translate(sources, {
+                    exportName: 'BEMHTML',
+                    devMode: process.env.BEMHTML_ENV == 'development'
+                });
+            });
+
+    },
+
+    getSuffixes: function() {
+        return ['bemhtml'];
+    },
+
+    getBuildSuffixes: function() {
+        return ['bemhtml.js'];
+    }
+
 };
